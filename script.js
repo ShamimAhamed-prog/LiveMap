f1cee8b8ceb0a6093bd5c6a926f31017e3954d99
Shamim Ahamed <Shamim.aiub2@gmail.com>
2020-10-01 01:19:15 +0600

    second commit

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
-var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+ var mymap = L.map('mapid').setView([51.505, -0.09], 25);
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
-    id: 'mapbox/streets-v11',
-    tileSize: 512,
+     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+          maxZoom: 18,
+        id:'mapbox/streets-v11',
+     tileSize: 512,
     zoomOffset: -1,
-    accessToken: 'your.mapbox.access.token'
-}).addTo(mymap);
+    accessToken: 'R40KmBReKynoWii9zPNecIiNMTulxIdaNh6LcOIZDw4'
+ }).addTo(mymap);
+
+var layerGroup = L.layerGroup().addTo(mymap);
+
+function testFunction1(){
+   layerGroup.clearLayers();
+   mymap.closePopup();
+   marker = L.marker([54.856, -110.18795]).addTo(layerGroup);
+   marker.bindPopup("Shamim").openPopup();
+
+}
